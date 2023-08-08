f4e9905be559e73e6f4b62bd2c41c4f8bc95b586
owen2345 <owenperedo@gmail.com>
2023-08-08 11:47:51 -0400

    feat: allow to drop triggers

--- lib/pgslice/cli/trigger.rb
+++ lib/pgslice/cli/trigger.rb
@@ -1,12 +1,18 @@
 module PgSlice
   class CLI
+    # pgslice trigger <table>
     desc "Manage triggers", "Allows to keep synced actual table with intermediate or retired"
-    option :swapped, type: :boolean, default: false, desc: "True if the table was already swapped (retired)"
+    option :swapped, type: :boolean, default: false, desc: "If true, the data will be synced to the retired table"
+    option :drop, type: :boolean, default: false, desc: "If true, the current trigger will be dropped without adding new one"
     def trigger(table)
       table = create_table(table)
       triggers_manager = TriggersManager.new(table, swapped: options[:swapped])
-      run_query triggers_manager.drop_triggers
-      run_query triggers_manager.build_triggers
+
+      if options[:drop]
+        run_query triggers_manager.drop_triggers
+      else
+        run_query triggers_manager.build_triggers
+      end
     end
   end
 end
